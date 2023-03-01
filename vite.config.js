74dc9ac9124746394e3ed29f7d04fe5154e63102
MartinsRoberto <robertomartinsramm@gmail.com>
2023-03-01 20:04:28 -0300

    ajustes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/2023-multistep_form',
+  base:'/react_multistep_form',
   plugins: [react()],
 })
